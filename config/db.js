449965eea86ea5cce97ed3fe7851c863efe469e2
Wesleybezerra3 <wesleybezerra770@gmail.com>
2025-02-20 02:32:47 -0300

    db config

--- config/db.js
+++ config/db.js
@@ -1,14 +1,21 @@
-const Sequelize = require('sequelize');
-const database = new Sequelize('capitulo_verde', 'root', 'Wes1018m.',{
-    dialect:'mysql',
-    host:'localhost'
-})
-try{
-    database.authenticate()
-    console.log('Banco de dados conectado!')
-}catch(err){
-    console.log('Errro ao conectar no banco de dados!!', err)
-}
+require('dotenv').config();
+const env = process.env;
+const Sequelize = require("sequelize");
+const database = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASS, {
+  dialect: "mysql",
+  port: env.DB_PORT,
+  host: env.DB_HOST,
+  logging: false,
+});
+const connectDB = async () => {
+  try {
+    await database.authenticate();
+    console.log("✅ Banco de dados conectado com sucesso!");
+  } catch (err) {
+    console.log("❌ Erro ao conectar no banco de dados:", err);
+  }
+};
 
+connectDB();
 
 module.exports = database;
