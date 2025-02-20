cfdaf99f5d8b0f74ef765264ed71fc71f440ee22
Wesleybezerra3 <wesleybezerra770@gmail.com>
2025-02-20 03:08:22 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,20 +1,26 @@
-require('dotenv').config();
-const env = process.env;
-const Sequelize = require("sequelize");
-const database = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASS, {
-  dialect: "mysql",
-  port: env.DB_PORT,
-  host: env.DB_HOST,
-  logging: false,
-});
-const connectDB = async () => {
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+
+const database = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    logging: false,
+  }
+);
+
+async function connectDB() {
   try {
     await database.authenticate();
     console.log("✅ Banco de dados conectado com sucesso!");
   } catch (err) {
-    console.log("❌ Erro ao conectar no banco de dados:", err);
+    console.error("❌ Erro ao conectar no banco de dados:", err);
   }
-};
+}
 
 connectDB();
 
